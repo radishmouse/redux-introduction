5834047fb28f0680bad3609d39d1c2da550d2e16
Chris Aquino <chrisaquino@radishmouse.local>
2020-02-11 10:27:35 -0500

    adds code for solutions, small and medium

--- src/index.js
+++ src/index.js
@@ -1,175 +1,80 @@
-// console.log('it is working. i am a robot')
-
-// "The bank" - state
-// Describe the ideal version of state
 // {
-//     amount: 100
+//     movieCount: 0
 // }
 
-// // if we added 1 to the amount, what would state look like?
+// const ADD_TO_MOVIE_COUNT = 'ADD_TO_MOVIE_COUNT'
 // {
-//     amount: 101
-// }
-
-
-// // "A transaction slip" - action
-// {
-//     type: 'INCREMENT'    
-// }
-
-// {
-//     type: 'DECREMENT'
-// }
-
-// {
-//     type: '🏄'
-// }
-
-// {
-//     amounts: [0]
-// }
-
-// {
-//     amounts: [1]
-// }
-
-// {
-//     type: INCREMENT,
-//     id: 0    
-// }
-
-// { 
-//     amounts: [54, 22, 10, 87] 
-// }
-
-// { 
-//     amounts: [54, 10, 87] 
+//     type: ADD_TO_MOVIE_COUNT 
 // }
 
 
 // {
-//     type: 'DEL_COUNTER'
-//     id: 1
+//     burritosEaten: 0
+// }
+
+// const ADD_TO_BURRITOS_EATEN = 'ADD_TO_BURRITOS_EATEN'
+// {
+//     type: ADD_TO_BURRITOS_EATEN
 // }
 
 
+// {
+//     favoriteSong: "happy birthday"
+// }
 
-import { 
-    createStore
-} from 'redux';
-// "The teller" - reducer function
-// reducers are always named for the state they manage.
-// They always receive the current state and the action
-// they're processing.
-const defaultState = { amounts: [43, 19, 84, 10, 3] };
+// const UPDATE_SONG = 'UPDATE_SONG'
+// {
+//     type: UPDATE_SONG,
+//     payload: {
+//         songName: 'unhappy birthday',
+//         updateDate: '2020-02-11'
+//     }
+// }
 
-// CREATE
-const ADD_COUNTER = 'ADD_COUNTER';
+// {
+//     ageOfHappyBirthdaySong: 99999999
+// }
 
-// UPDATE
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
+// {
+//     coffees: 0
+// }
 
-// DELETE
-const DEL_COUNTER = 'DEL_COUNTER';
+// const INCREMENT_COFFEE_COUNT = 'INCREMENT_COFFEE_COUNT';
+// {
+//     type: INCREMENT_COFFEE_COUNT
+// }
 
+const defaultLunchState = {
+    lunch: "burrito"
+}
 
-function counter(state=defaultState, action) {
-    console.log('Somebody called counter()');
+const UPDATE_LUNCH_ITEM = 'UPDATE_LUNCH_ITEM'
+
+// example action that modifies lunch item
+// {
+//     type: UPDATE_LUNCH_ITEM,
+//     payload: {
+//         itemName: "hot dog"
+//     }
+// }
+
+// example action provided by redux
+// when it creates the store
+// {
+//     type: '@@REDUX/INITaasdf78ashfaskufdy'
+// }
+
+function lunch(state=defaultLunchState, action) {
     const newState = { ...state };
-    
-    switch(action.type) {
-        case INCREMENT:
-            newState.amounts[action.id] = state.amounts[action.id] + 1;
-            break;
-        case DECREMENT:
-            newState.amounts[action.id] = state.amounts[action.id] - 1;
-            break;
-        case ADD_COUNTER:
-            newState.amounts.push(0);
-            break;
-        case DEL_COUNTER:
-            newState.amounts.splice(action.id, 1);
+    switch (action.type) {
+        case UPDATE_LUNCH_ITEM:
+            newState.lunch = action.payload.itemName;
             break;
         default:
+            console.log('Did not match');
             break;
     }
     return newState;
 }
 
-// You give it a reducer, it gives you a "store".
-// The store is an object that manages your state 
-// using your reducer.
-const store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-// "Push notifications" - subscribe to changes in the store
-store.subscribe(() => {
-    console.log(`The state is now:`);
-    // console.table();
-    console.table(store.getState());
-});
 
-
-function actionIncrement(id) {
-    return {
-        type: INCREMENT,
-        id
-    }
-}
-
-function actionDecrement(id) {
-    return {
-        type: DECREMENT,
-        id
-    }
-}
-
-function actionAddCounter() {
-    return {
-        type: ADD_COUNTER
-    }
-}
-
-function actionDelCounter(id) {
-    return {
-        type: DEL_COUNTER,
-        id
-    }
-}
-
-store.dispatch(actionAddCounter())
-store.dispatch(actionDelCounter(3));
-store.dispatch(actionDelCounter(0));
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-// store.dispatch(actionAddCounter())
-
-
-// Let's give the store some actions to process.
-// store.dispatch(actionIncrement(0));
-// store.dispatch(actionIncrement(0));
-// store.dispatch(actionIncrement(0));
-// store.dispatch(actionIncrement(2));
-// store.dispatch(actionIncrement(2));
-// store.dispatch(actionDecrement(0));
-// store.dispatch(actionDecrement(1));
-// store.dispatch(actionDecrement(4));
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
